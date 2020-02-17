d2561ddad7b722bfa75a53131b9c0c3cb9d4ea34
KMA71 <kma-work@yandex.ru>
2020-02-17 15:40:44 +0300

    creating main pages for roles mechanism

---
+++ src/test/java/e2e/GozCreateDeal.java
@@ -0,0 +1,27 @@
+package e2e;
+
+import base.SetupEnv;
+import landing.CarLoginPage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import rolepages.GozMain;
+import roles.GozRole;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Execution(ExecutionMode.CONCURRENT)
+public class GozCreateDeal extends SetupEnv {
+
+    @Test
+    @DisplayName("Заведение заявки ГОЗ-ом")
+    void createDeal() {
+
+        CarLoginPage carLoginPage = new CarLoginPage(driver);
+        GozMain gozMain = carLoginPage.auth("sale_point", "sale_point", new GozRole());
+
+        assertTrue(true);
+    }
+
+}
